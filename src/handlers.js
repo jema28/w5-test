27e8340f6411fc0622acbbe115bbb406f67f7d37
DuaaH <dodohalaiqa.1@gmail.com>
2019-07-21 13:33:17 +0300

    solution

--- src/handlers.js
+++ src/handlers.js
@@ -1,47 +1,61 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
+const request = require("request");
 
 const handleHomeRoute = (request, response) => {
-  const filePath = path.join(__dirname, '..', 'public', 'index.html');
+  const filePath = path.join(__dirname, "..", "public", "index.html");
   fs.readFile(filePath, (error, file) => {
     if (error) {
       console.log(error);
-      response.writeHead(500, {'Content-Type': 'text/html'});
+      response.writeHead(500, { "Content-Type": "text/html" });
       response.end("<h1>Sorry, we've had a problem on our end</h1>");
     } else {
-      response.writeHead(200, {'Content-Type': 'text/html'});
+      response.writeHead(200, { "Content-Type": "text/html" });
       response.end(file);
     }
   });
 };
 
 const handlePublic = (request, response, url) => {
-  const extension = url.split('.')[1];
+  const extension = url.split(".")[1];
   const extensionType = {
-    html: 'text/html',
-    css: 'text/css',
-    js: 'application/javascript',
-    ico: 'image/x-icon',
-    jpg: 'image/jpeg',
-    png: 'image/png'
+    html: "text/html",
+    css: "text/css",
+    js: "application/javascript",
+    ico: "image/x-icon",
+    jpg: "image/jpeg",
+    png: "image/png"
   };
-  const filePath = path.join(__dirname, '..', url);
+  const filePath = path.join(__dirname, "..", url);
 
   fs.readFile(filePath, (error, file) => {
     if (error) {
       console.log(error);
-      response.writeHead(404, {'Content-Type': 'text/html'});
-      response.end('<h1>404 file not found</h1>');
+      response.writeHead(404, { "Content-Type": "text/html" });
+      response.end("<h1>404 file not found</h1>");
     } else {
-      response.writeHead(200, {'Content-Type': extensionType[extension]});
+      response.writeHead(200, { "Content-Type": extensionType[extension] });
       response.end(file);
     }
   });
 };
 
-const handleAPI = (request, response) => {
+const handleAPI = (req, response) => {
   // add code here!
-}
+  request(
+    `http://www.omdbapi.com/?s=star-wars&apikey=21bfd2de`,
+    { json: true },
+    (err, res, body) => {
+      if (err) {
+        return console.log(err);
+      } else {
+        //  console.log(body);
+        // response.end(body);
+        response.end(JSON.stringify(body));
+      }
+    }
+  );
+};
 
 module.exports = {
   handleHomeRoute,
--- src/server.js
+++ src/server.js
@@ -1,9 +1,10 @@
-const http = require('http');
-const router = require('./router')
+const http = require("http");
+const router = require("./router");
 
-const port = 4000;
+const port = 3002;
 
 const server = http.createServer(router);
 
-server.listen(port,  () => console.log(`server up and running on localhost:${port}`));
-
+server.listen(port, () =>
+  console.log(`server up and running on localhost:${port}`)
+);

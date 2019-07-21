34f7b8372dd781428f40d2d60f716e3ee82a66c3
nadeen123 <nadeenshaheen.shahin@gmail.com>
2019-07-21 21:42:14 +0300

    solution

--- src/handlers.js
+++ src/handlers.js
@@ -1,47 +1,58 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
+const request = require("request");
 
 const handleHomeRoute = (request, response) => {
-  const filePath = path.join(__dirname, '..', 'public', 'index.html');
+  const filePath = path.join(__dirname, "..", "public", "index.html");
   fs.readFile(filePath, (error, file) => {
     if (error) {
       console.log(error);
-      response.writeHead(500, {'Content-Type': 'text/html'});
+      response.writeHead(500, { "Content-Type": "text/html" });
       response.end("<h1>Sorry, we've had a problem on our end</h1>");
     } else {
-      response.writeHead(200, {'Content-Type': 'text/html'});
+      response.writeHead(200, { "Content-Type": "text/html" });
       response.end(file);
     }
   });
 };
 
 const handlePublic = (request, response, url) => {
-  const extension = url.split('.')[1];
+  const extension = url.split(".")[1];
   const extensionType = {
-    html: 'text/html',
-    css: 'text/css',
-    js: 'application/javascript',
-    ico: 'image/x-icon',
-    jpg: 'image/jpeg',
-    png: 'image/png'
+    html: "text/html",
+    css: "text/css",
+    js: "application/javascript",
+    ico: "image/x-icon",
+    jpg: "image/jpeg",
+    png: "image/png"
   };
-  const filePath = path.join(__dirname, '..', url);
+  const filePath = path.join(__dirname, "..", url);
 
   fs.readFile(filePath, (error, file) => {
     if (error) {
       console.log(error);
-      response.writeHead(404, {'Content-Type': 'text/html'});
-      response.end('<h1>404 file not found</h1>');
+      response.writeHead(404, { "Content-Type": "text/html" });
+      response.end("<h1>404 file not found</h1>");
     } else {
-      response.writeHead(200, {'Content-Type': extensionType[extension]});
+      response.writeHead(200, { "Content-Type": extensionType[extension] });
       response.end(file);
     }
   });
 };
 
-const handleAPI = (request, response) => {
-  // add code here!
-}
+const handleAPI = (req, res, url) => {
+  url = `http://www.omdbapi.com/?s=star-wars&apikey=1fa7206d`;
+  request(url, (error, response, body) => {
+    if (error) {
+      console.log(error);
+      res.writeHead(404, { "Content-Type": "text/html" });
+      res.end("<h1>404 file not found</h1>");
+    }
+    res.writeHead(200, { "Content-Type": "application/json" });
+    const data = JSON.parse(body);
+    res.end(JSON.stringify(data));
+  });
+};
 
 module.exports = {
   handleHomeRoute,
